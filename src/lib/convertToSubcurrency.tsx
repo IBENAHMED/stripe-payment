4704a46f71befbc7f00b8c0271b98bca7f508fb3
Reda IBENAHMED <ibenahmed.reda05@gmail.com>
2024-09-28 18:58:26 +0100

    first commit

---
+++ src/lib/convertToSubcurrency.tsx
@@ -0,0 +1,10 @@
+// function convertToSubcurrency(amount: any, factor = 100) {
+//     return Math.round(amount * factor);
+// }
+// export default convertToSubcurrency;
+
+function convertToSubcurrency(amount: number, factor = 100) {
+    return Math.round(amount * factor);
+}
+
+export default convertToSubcurrency;
